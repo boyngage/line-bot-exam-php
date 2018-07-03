b83fa6b2384e5fb81cdcb54cc8c0552453de7227
boyngage <klongchu@gmail.com>
2018-07-03 11:42:33 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -45,11 +45,11 @@ if (!is_null($events['events'])) {
 			curl_close($ch);
             //echo $result . "\r\n";
 			 $channelSecret = '335ff1c117bcb003169d73477f504b77';
-			 $idPush = $result;
+			 $idPush = 'xxxxxx';
 			 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 			 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 			 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
-			 $response = $bot->pushMessage($idPush, $textMessageBuilder);
+			 $response = $bot->pushMessage('U2689a511d9d51e000b9f9e852d95649b', $textMessageBuilder);
 
 			 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 
